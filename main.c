65b10ce9425a1294c2a73c204b947062867696d2
haroun73 <138124845+haroun73@users.noreply.github.com>
2023-09-29 15:07:29 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// structure 
+struct  moteur {
+
+	bool démarer;
+	bool utiliser;
+	bool arreter;
+};
+// fontions demarer
+void DémarerMoteur(struct moteur);
+if (moteur->démarre) {
+	moteur->déemarre = true;
+	printf("Le moteur a été démarré.\n")
+}
+else {
+	printf("le moteur est déja démaré.\n")
+}
+// fontion utiliser
+void UtiliserMoteur(struct moteur);
+//fontion arreter
+void ArreterMoteur(struct moteur);
