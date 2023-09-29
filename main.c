07cebdd7816d2ac7d12d1e10904c67e63a57e195
haroun73 <138124845+haroun73@users.noreply.github.com>
2023-09-29 16:29:40 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,87 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// structure 
-struct  moteur {
-
-	bool démarer;
-	bool utiliser;
-	bool arreter;
+// Structure 
+struct Moteur {
+    bool Demarrer;
+    bool Utiliser;
 };
-// fontions demarer
-void DémarerMoteur(struct moteur);
-if (moteur->démarre) {
-	moteur->déemarre = true;
-	printf("Le moteur a été démarré.\n")
+
+// Fonction  démarrer 
+void demarrerMoteur(struct Moteur* moteur) {
+    if (moteur->Demarrer) {
+        moteur->Utiliser = true;
+        printf("Le moteur a été démarré.\n");
+    }
+    else {
+        printf("Le moteur est déjà démarré.\n");
+    }
 }
-else {
-	printf("le moteur est déja démaré.\n")
+
+// Fonction utiliser
+void utiliserMoteur(struct Moteur* moteur) {
+    if (moteur->Demarrer) {
+        if (moteur->Utiliser) {
+            moteur->Utiliser = true;
+            printf("Le moteur est en utilisation.\n");
+        }
+        else {
+            printf("Le moteur est déjà en utilisation.\n");
+        }
+    }
+    else {
+        printf("Le moteur n'est pas démarré. Veuillez le démarrer d'abord.\n");
+    }
 }
-// fontion utiliser
-void UtiliserMoteur(struct moteur);
-//fontion arreter
-void ArreterMoteur(struct moteur);
+
+// Fonction  arrêter 
+void arreterMoteur(struct Moteur* moteur) {
+    if (moteur->Demarrer) {
+        if (moteur->Utiliser) {
+            moteur->Utiliser = false;
+            printf("Le moteur a été arrêté.\n");
+        }
+        else {
+            printf("Le moteur est déjà arrêté.\n");
+        }
+    }
+    else {
+        printf("Le moteur n'est pas démarré.\n");
+    }
+}
+
+int main() {
+    struct Moteur monMoteur;
+    monMoteur.Utiliser = false;
+    monMoteur.Utiliser = false;
+
+    int choix;
+    while (1) {
+        printf("\nMenu :\n");
+        printf("1. Démarrer le moteur\n");
+        printf("2. Utiliser le moteur\n");
+        printf("3. Arrêter le moteur\n");
+        printf("4. Quitter l'application\n");
+        printf("Entrez votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+        case 1:
+            demarrerMoteur(&monMoteur);
+            break;
+        case 2:
+            utiliserMoteur(&monMoteur);
+            break;
+        case 3:
+            arreterMoteur(&monMoteur);
+            break;
+        case 4:
+            printf("Quitter l'application.\n");
+            return 0;
+        default:
+            printf("Choix non valide. Veuillez réessayer.\n");
+        }
+    }
+
+    return 0;
+}
